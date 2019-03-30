3ffa4323fad838e30dd1ce86383490c6621cfdbc
gelizondomora <40377600+gelizondomora@users.noreply.github.com>
2019-03-30 12:12:37 -0600

    Add files via upload

---
+++ practica_2.py
@@ -0,0 +1,16 @@
+# Esta es al practica para escribir dos lineas
+# en consola
+
+"""
+
+Comentario de multiples lineas
+util para explicar mas detalles
+
+"""
+
+print("Hola a todos!")
+print("buenos dias")
+
+#Varias lineas en un solo print
+print("mi primera linea \nmi segunda linea")
+    print("hola de nuevo")
