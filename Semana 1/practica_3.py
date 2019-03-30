471502285498d39b3f11693c6c77b77e7bcd922e
gelizondomora <40377600+gelizondomora@users.noreply.github.com>
2019-03-30 12:15:36 -0600

    Add files via upload

---
+++ Semana 1/practica_3.py
@@ -0,0 +1,16 @@
+#revisar los tipos de datos
+
+#numero entero
+#definir numero entero
+
+123456789
+mi_variable = 123456789
+
+#Caso 1
+print("El valor de la variable llamada 'mi_variable es' ", mi_variable)
+
+#Caso 2
+print('El valor de la variable llamada "mi_variable es" ', mi_variable)
+
+#Caso 3
+print("El valor de la variable llamada \'mi_variable es\' ", mi_variable)
---
+++ Semana 1/practica_4.py
@@ -0,0 +1,9 @@
+#cambios de tipo de dato
+
+mi_variable = 10
+
+print("El tipo es ",type(mi_variable), mi_variable)
+
+mi_variable = "10"
+
+print("El tipo es ",type(mi_variable), mi_variable)
