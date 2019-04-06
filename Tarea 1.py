d7a2cd74796fd1d2fc8b015c0d1316fbcb106538
gelizondomora <40377600+gelizondomora@users.noreply.github.com>
2019-04-05 21:58:01 -0600

    Tarea 1 Semana 1

---
+++ Tarea 1.py
@@ -0,0 +1,6 @@
+import math as math
+
+value = math.pi
+star = "*******"
+print("(%.1s)(%.6f)" %(star,value),"(%.2s)(%.5f)" %(star,value),"(%.3s)(%.4f)" %(star,value),
+      "(%.4s)(%.3f)" %(star,value),"(%.5s)(%.2f)" %(star,value),"(%.6s)(%.1f)" %(star,value))
